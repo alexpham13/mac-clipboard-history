462b10e5cad62ea5681dc6061c72d137b06b7e25
alexpham13 <alexbaopham@gmail.com>
2026-09-03 14:29:18 -0500

    Create initial Xcode project

---
+++ Clipboard History/Clipboard History/Clipboard_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clipboard_HistoryApp.swift
+//  Clipboard History
+//
+//  Created by Alex Pham on 9/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Clipboard_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
