f6e5b2b7e2a430b8d3ef65208f5d15e05ea8820b
alexpham13 <alexbaopham@gmail.com>
2026-09-23 20:15:20 -0500

    Add image support to clipboard history

--- Clipboard History/Clipboard History/ContentView.swift
+++ Clipboard History/Clipboard History/ContentView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 import AppKit
 
+enum ClipboardType: Codable {
+    case text
+    case image
+}
+
+struct ClipboardItem: Codable, Identifiable {
+    var type: ClipboardType
+    var text: String?
+    var imageData: Data?
+    var id = UUID()
+    
+}
 struct ContentView: View {
-    @State var clipboardHistory: [String] = []
+    @State var clipboardHistory: [ClipboardItem] = []
     @State var previousChangeCount: Int = 0
     @State var clipboardTimer: Timer? = nil
     let pasteboard = NSPasteboard.general
@@ -18,10 +30,20 @@ struct ContentView: View {
         if previousChangeCount != pasteboard.changeCount {
             let copiedText = pasteboard.string(forType: .string)
             if let text = copiedText {
-                if let index = clipboardHistory.firstIndex(of: text) {
+                if let index = clipboardHistory.firstIndex(where: {$0.text == text}) {
                     clipboardHistory.remove(at: index)
                 }
-                clipboardHistory.insert(text, at: 0)
+                                clipboardHistory.insert(ClipboardItem (type: .text, text: text, imageData: nil), at: 0)
+                if clipboardHistory.count > 50 {
+                    clipboardHistory.removeLast()
+                }
+                saveHistory()
+            }
+            if let imageData = pasteboard.data(forType: .tiff) {
+                if let index = clipboardHistory.firstIndex(where: { $0.imageData == imageData }) {
+                    clipboardHistory.remove(at: index)
+                }
+                clipboardHistory.insert(ClipboardItem (type: .image, text: nil, imageData: imageData), at: 0)
                 if clipboardHistory.count > 50 {
                     clipboardHistory.removeLast()
                 }
@@ -31,18 +53,30 @@ struct ContentView: View {
         }
     }
     func saveHistory() {
-        UserDefaults.standard.set(clipboardHistory, forKey: "savedClipboardHistory")
+        if let data = try? JSONEncoder().encode(clipboardHistory) {
+            UserDefaults.standard.set(data, forKey: "savedClipboardHistory")
+        }
     }
     func loadHistory() {
         if let savedHistory =
-            UserDefaults.standard.array(forKey: "savedClipboardHistory") as? [String] {
-            clipboardHistory = savedHistory
+            UserDefaults.standard.data(forKey: "savedClipboardHistory") {
+            if let decodedHistory = try?
+                JSONDecoder().decode([ClipboardItem].self, from: savedHistory) {
+                clipboardHistory = decodedHistory
+            }
         }
     }
+    
     func rowBackground(item: String) -> Color {
         if item == pasteboard.string(forType: .string) {
             return Color.blue.opacity(0.15)
-        }else{ return Color.clear
+        } else { return Color.clear
+        }
+    }
+    func imageRowBackground(imageData: Data) -> Color {
+        if imageData == pasteboard.data(forType: .tiff) {
+            return Color.blue.opacity(0.15)
+        } else { return Color.clear
         }
     }
     
@@ -75,44 +109,64 @@ struct ContentView: View {
             
             ScrollView {
                 
-                ForEach(clipboardHistory, id: \.self) {item in
+                ForEach(clipboardHistory) {item in
                     
-                    HStack {
-                        
-                        Button {
-                            pasteboard.clearContents()
-                            pasteboard.setString(item, forType: .string)
-                        } label: {
-                            Text(item)
-                                .lineLimit(2)
-                                .multilineTextAlignment(.leading)
-                        }
-                        .buttonStyle(.plain)
-                        
-                        Spacer()
-                        
-                        Button {
-                            if let index = clipboardHistory.firstIndex(of: item) {
-                                clipboardHistory.remove(at: index)
-                                saveHistory()
-                            }
-                        } label: {
-                            Label ("Delete", systemImage: "trash")
+                    if let text = item.text {
+                        HStack {
                             
+                            Button {
+                                pasteboard.clearContents()
+                                pasteboard.setString(text, forType: .string)
+                            } label: {
+                                Text(text)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(.leading)
+                            }
+                            .buttonStyle(.plain)
+                            
+                            Spacer()
+                            
+                            Button {
+                                if let index = clipboardHistory.firstIndex(where: { $0.id == item.id}) {
+                                    clipboardHistory.remove(at: index)
+                                    saveHistory()
+                                }
+                            } label: {
+                                Label ("Delete", systemImage: "trash")
+                                
+                            }
+                        }
+                        
+                        .padding(.vertical, 8)
+                        .background(rowBackground(item: text))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 16)
+                        Divider()
+                    }
+                    if let imageData = item.imageData {
+                        if let image = NSImage(data: imageData) {
+                            Button {
+                                pasteboard.clearContents()
+                                pasteboard.setData(imageData, forType: .tiff)
+                            } label: {
+                                HStack {
+                                    
+                                    Image(nsImage: image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxHeight: 150)
+                                    Spacer()
+                                    
+                                }
+                                .background(imageRowBackground(imageData: imageData))
+                            }
                         }
                     }
-                    .padding(.vertical, 8)
-                    .background(rowBackground(item: item))
-                    .cornerRadius(10)
-                    .padding(.horizontal, 16)
-                    Divider()
-                    
                 }
             }
                    }
+        
         .frame(minWidth: 500)
-        
-        
         
         .onAppear {
             loadHistory()
